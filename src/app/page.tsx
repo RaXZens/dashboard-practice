1c28210d435d318c8d8c2e29ae271237bd08d300
RaXZens <leekung119@gmail.com>
2025-07-11 16:24:35 +0700

    first commit new branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 
-import AddAreaChart from "@/components/controller/AddAreaChart";
-import AddBarCharts from "@/components/controller/AddBarCharts";
-import AddPieChart from "@/components/controller/AddPieChart";
-import AppBarChart from "@/components/controller/AppBarChart";
 import NavbarPublic from "@/components/ui/navbar";
-import UsersPage from "@/components/controller/Cruid";
-import Link from "next/link";
+
 
 
 async function Homepage () {
