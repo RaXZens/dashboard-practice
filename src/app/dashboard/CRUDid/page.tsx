c6ddaca5e3fe51c23c3ddeb1c346ba03b15f00d9
RaXZens <leekung119@gmail.com>
2025-07-21 21:18:29 +0700

    change cruid to be CRUDid

--- src/app/dashboard/CRUDid/page.tsx
+++ src/app/dashboard/CRUDid/page.tsx
@@ -1,4 +1,4 @@
-import UsersPage from "@/components/Cruid";
+import UsersPage from "@/components/CRUDid";
 import { authOption } from "@/app/lib/auth"
 import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
--- src/components/AppSideBar.tsx
+++ src/components/AppSideBar.tsx
@@ -33,8 +33,8 @@ const items = [
     icon: Home,
   },
   {
-    title: "Cruid",
-    url: "/dashboard/cruid",
+    title: "CRUDid",
+    url: "/dashboard/CRUDid",
     icon: Users,
   },
 ];
